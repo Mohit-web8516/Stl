ed17dfd2163619fa4780c62bfa7adb589753e772
mohit-8516 <mohitshrivastav429@gmail.com>
2026-06-02 09:08:43 +0530

    Updated

---
+++ STL/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     pair<int, int> p ;
+
+//     p.first = 10;
+//     p.second = 20;
+
+//     cout << p.first << " " << p.second;
+//     return 0;
+// }
