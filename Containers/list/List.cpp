2d592e1b8bba179807d73afeb3b5747c78af81ec
mohit-8516 <mohitshrivastav429@gmail.com>
2026-06-02 12:14:35 +0530

    List

---
+++ Containers/list/List.cpp
@@ -0,0 +1,14 @@
+//LIST CREATION
+
+
+// #include <iostream>
+// #include <list>
+// using namespace std;
+// int main(){
+//     list <int> l = {10,30,50,70};
+
+//     for(auto x:l)
+//     {
+//         cout << x << " ";
+//     }
+// }
