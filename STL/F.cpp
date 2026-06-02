43cd5c3e807e5f1d1ed1264cd35512c67c9a9b2b
mohit-8516 <mohitshrivastav429@gmail.com>
2026-06-02 09:07:52 +0530

    first

--- STL/F.cpp
+++ STL/F.cpp
@@ -1,1 +1,14 @@
-#i
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<double> v = {5.5,2,8,1};
+
+    sort(v.begin(), v.end());
+
+    for(int x : v)
+        cout << x << " ";
+}
