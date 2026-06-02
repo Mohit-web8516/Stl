578bd084f89b326e7d4ac9754ffc985d5d18c22c
mohit-8516 <mohitshrivastav429@gmail.com>
2026-06-02 12:13:53 +0530

    Updated

--- Containers/tempCodeRunnerFile.cpp
+++ Containers/tempCodeRunnerFile.cpp
@@ -1,2 +1,17 @@
-0);
-    // v.insert(v.begin(), copy.begin
+am>
+// #include <set>
+// using namespace std;
+
+// int main()
+// {
+//     multiset<int> ms={10,10,20};
+
+//     auto it=ms.find(10);
+
+//     ms.erase(it);
+
+//     for(auto x:ms)
+//     {
+//         cout<<x<<" ";
+//     }
+// }
